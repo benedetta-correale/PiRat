33056721e75dab9d04086c64ebdbfa52466fcac1
Guglipadella <guglielmosellitri2003@gmail.com>
2025-06-29 18:11:40 +0200

    spero la nebbia vada finalmente bene + prefab trappole finiti
    ho bisogno di soldi cazzo

--- Assets/_Imported_Packages/AOSFogWar/csFogWar.cs
+++ Assets/_Imported_Packages/AOSFogWar/csFogWar.cs
@@ -217,7 +217,7 @@ namespace FischlWorks_FogWar
         [Range(0, 1)]
         private float fogPlaneAlpha = 1;
         [SerializeField]
-        [Range(1, 5)]
+        [Range(1, 100)]
         private float fogLerpSpeed = 2.5f;
         public bool keepRevealedTiles = false;
         [ShowIf("keepRevealedTiles")]
